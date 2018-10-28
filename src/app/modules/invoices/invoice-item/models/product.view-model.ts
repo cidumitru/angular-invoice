a1010d0e267cfbee845d677bc307d475661f26ba
Dumitru <cusnir.dumitru@gmail.com>
2018-10-29 00:13:33 +0200

    Product View Model

---
+++ src/app/modules/invoices/invoice-item/models/product.view-model.ts
@@ -0,0 +1,17 @@
+import {IProduct} from '../../../../core/shared/interfaces/product.interface';
+
+export interface IProductViewModel extends IProduct {
+  quantity: number;
+}
+
+export class ProductViewModel {
+  id: number;
+  name: string;
+  code: string;
+  price: number;
+  quantity: number;
+
+  constructor(product: IProductViewModel) {
+    Object.assign(this, product);
+  }
+}
